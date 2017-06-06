ee5ce18e30106a62dfdd6c3c7d7ff0cb90c6d000
elgururincon <davidrincont@gmail.com>
2017-06-05 21:42:16 -0500

    Update triqui.py

--- triqui.py
+++ triqui.py
@@ -9,6 +9,8 @@
 # DataStructuresCourse is free software: you can redistribute it and/or modify it under the terms of the
 # GNU General Public License as published by the Free Software Foundation, version 2.
 */
+#!/usr/env/python
+# -*- coding: utf-8 -*
 
 
 tablero = [1,2,3,4,5,6,7,8,9]                               # creacion del tablero
